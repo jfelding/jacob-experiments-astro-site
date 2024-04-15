f1a51ea5f10acb07e9bce297ddb5e5e34703865a
Jacob Felding <jfelding@gmail.com>
2024-04-15 17:07:19 +0200

    removed astro-imagetools for compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import { astroImageTools } from "astro-imagetools";
-
 import decapCmsOauth from "astro-decap-cms-oauth";
+import cloudflare from "@astrojs/cloudflare";
+
+let adapter = cloudflare();
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jacob-experiments.com",
-  integrations: [mdx(), sitemap(), tailwind(), astroImageTools, decapCmsOauth()],
-  output: "hybrid"
+  integrations: [mdx(), sitemap(), tailwind(), decapCmsOauth()],
+  output: "hybrid",
+  adapter: adapter
 });
