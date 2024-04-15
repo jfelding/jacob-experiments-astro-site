416eedea02f9347645253aa9383ed157bbf13ea4
Jacob Felding <jfelding@gmail.com>
2024-04-15 18:36:45 +0200

    removed cloudflare adapter again after using netlify identity instead

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,9 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import decapCmsOauth from "astro-decap-cms-oauth";
-import cloudflare from "@astrojs/cloudflare";
-
-let adapter = cloudflare();
-
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jacob-experiments.com",
-  integrations: [mdx(), sitemap(), tailwind(), decapCmsOauth()],
-  output: "hybrid",
-  adapter: adapter
+  integrations: [mdx(), sitemap(), tailwind()],
 });
