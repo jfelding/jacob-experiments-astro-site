c74f4cc5da188d27e674f9c79822c4c24a17b81b
Jacob Ungar Felding <79907020+jfelding@users.noreply.github.com>
2024-04-15 19:06:12 +0200

    decap-integration (#10)
    * initial decap oauth configuration
    
    * removed astro-imagetools for compatibility
    
    * alternative setup with netlify identity
    
    * removed cloudflare adapter again after using netlify identity instead
    
    * removed imagetools, but otherwise reverted packages
    
    * updated asset
    
    * removed html asset
    
    * updated packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jacob-experiments.com",
-  integrations: [mdx(), sitemap(), tailwind(), astroImageTools],
-
+  integrations: [mdx(), sitemap(), tailwind()],
 });
